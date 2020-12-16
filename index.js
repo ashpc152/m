27f89c7461c7ae1bf67bea1c8659b8ed18dc4739
ashpc152 <76096405+ashpc152@users.noreply.github.com>
2020-12-16 14:28:38 +0530

    Add initial m website files

---
+++ index.js
@@ -0,0 +1,53 @@
+
+  /*$("#haal").click(function(){
+    aud = new Audio("music/Haal Kaisa Hai Janaab Ka.mp3");
+    aud.play();
+    });
+
+    $("#khat").click(function(){
+      aud = new Audio("music/Likhe Jo Khat Tujhe _ Sanam.mp3");
+      aud.play();
+      });
+
+      $("#uski").click(function(){
+        aud = new Audio("music/Taarif Karoon _ Sanam.mp3");
+        aud.play();
+        });
+
+      $("#awara").click(function(){
+        aud = new Audio("music/Hai Apna Dil To Awara.mp3");
+        aud.play();
+      });
+
+      $("#jigar").click(function(){
+        aud = new Audio("music/Jaane Kahan Mera Jigar Gaya Ji.mp3");
+        aud.play();
+      });
+
+      $("#balma").click(function(){
+        aud = new Audio("music/lichi.mp3");
+        aud.play();
+      });
+
+      $("#charche").click(function(){
+        aud = new Audio("music/charche.mp3");
+        aud.play();
+      });
+
+      $("#haari").click(function(){
+        aud = new Audio("music/Achha Ji Main Haari Chalo.mp3");
+        aud.play();
+      });
+
+      $("#hpau").click(function(){
+        aud.pause();
+      });
+
+*/
+$(".mylist").on("mouseover", function(){
+  this.style.color = "#ff414d";
+  });
+
+$(".mylist").on("mouseout", function(){
+    this.style.color = "black";
+    });
